19f3aecade604afe99c703f980b9ba656fbdfce3
Josh Watson <irishwatsonj>
2022-01-14 18:59:10 -0600

    finished coding the main python; now creats a txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,5 @@
+from cgitb import text
+from inspect import ClosureVars
 import os
 import csv
 import pathlib
@@ -12,25 +14,36 @@ with open(budget_path) as interactor:
     reader = csv.reader(interactor, delimiter = ",")
     header = next(interactor)
 
+    #variable for storing the number of months
     months = 0
+
+    #variable for storing the total profits
     total_profit = 0
 
+    #these lists will eventually hold all the values from the csv
+    #profits -> profit_list and months -> month_list
     profit_list = []
     month_list = []
 
+    #reads the csv file
     for row in reader:
+        
+        #counts the number of months
         months += 1
+
+        #sums all of th profits together
         total_profit += int(row[1])
 
-        profit_list.append(int(row[1]))
-        month_list.append(str(row[0]))
+        
+        profit_list.append(int(row[1])) #adds profit value to a list
+        month_list.append(str(row[0])) #adds the months to a list
 
 
 #uses a while loop to create a list of the profit changes
 #also saves the highest and lowest change in profits between months
 #and will record the date in which the highest/lowest change occured
 
-profit_changes = []
+profit_changes = [] #used for storing all of the changes in profits between months
 largest_increase = 0
 largest_decrease = 0
 
@@ -59,14 +72,23 @@ while x < months:
 #and rounds to 2 decimal places
 average_change = round(sum(profit_changes) / len(profit_changes),2)
 
+output_path = os.path.join(pathlib.Path(__file__).parent.resolve(), "analysis", "analysis.txt" )
 
+output_line_1 = "Total Months: " + str(months)
+output_line_2 = "Total: $" + str(total_profit)
+output_line_3 = "Average Change: " + str(average_change)
+output_line_4 = "Greatest Increase in Profits: " + str(largest_increase) + " " + str(largest_increase_month)
+output_line_5 = "Greatest Decrease in Profits: " + str(largest_decrease) + " " + str(largest_decrease_month)
+
+full_output = ["Financial Analysis", "", output_line_1, output_line_2, output_line_3, output_line_4, output_line_5]
+
+with open(output_path, 'w') as output_interactor:
+
+    for line in full_output:
+        output_interactor.write(line)
+        output_interactor.write("\n")
         
 
 
-print(f"Over a period of {months} months")
-print(f"we made ${total_profit} in net profit!")
-
-print(f"The average change in our profits across months was {average_change}.")
-print(f"The largest increase in profits was {largest_increase} in {largest_increase_month} and the largest decrease was {largest_decrease} in {largest_decrease_month}.")
-
-
+for line in full_output:
+    print(line)
